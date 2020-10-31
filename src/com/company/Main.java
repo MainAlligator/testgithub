2fc628b93af92f3f1b91950ac6607dcb83a71579
Alexi Krol <krol.mats@gmail.com>
2020-10-31 19:39:36 +0200

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String Name = "Ivan";
+        int age = 40;
+        System.out.println(Name +" + "+ age);
+        
+	// write your code here
+    }
+}
+
