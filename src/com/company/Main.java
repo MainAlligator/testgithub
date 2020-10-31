2da854ca67286ff817914844225f9afa485127ce
Alexi Krol <krol.mats@gmail.com>
2020-10-31 19:48:02 +0200

    New names

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         String Name = "Ivan";
+        String Name2 = "Losha";
         int age = 40;
-        System.out.println(Name +" + "+ age);
+        int ageLosha = 50;
+        System.out.println(Name +" + "+ age + " + " + Name2 + " + " + ageLosha);
         
 	// write your code here
     }
